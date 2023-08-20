195569b99ee3e00cd7629330e40c6c437569d765
trieu nguyen <trieunguyen912@gmail.com>
2023-08-20 12:31:15 +0700

    rafactor(UI) : margin buttom

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -6,10 +6,9 @@ const AppButton = ({ text }) => {
     <Grid>
       <Button
         className={styles.btn}
-        sx={{ borderRadius: "5px", backgroundColor: "#0B8DCD" , color:"white" , p:{xs:0, sm:2}}}
+        sx={{ borderRadius: "5px", backgroundColor: "#0B8DCD" , color:"white" , p:{xs:1, sm:2}}}
       >
       {text}
-       
       </Button>
     </Grid>
   );
