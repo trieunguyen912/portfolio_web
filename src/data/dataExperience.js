6b7ba37f9baf6c85b1623455759e8318bb5867b2
trieu nguyen <trieunguyen912@gmail.com>
2023-08-21 14:58:47 +0700

    rafactor(ui) : fix css

---
+++ src/data/dataExperience.js
@@ -0,0 +1,59 @@
+const dataExperience = [
+  {
+    header: "👨‍💻 Technical background ",
+    dataContent: [
+      {
+        content:
+          "• Developed Chrome Extension using ReactJS/Webpack for Insider Web Scrapping usage increasing the productivity of content managers by 85%",
+      },
+      {
+        content:
+          "• Team Leading Backend Side of the SSO(Single Sign On), Mollys Services and Ecommerce Products",
+      },
+      {
+        content:
+          " • Building better WordPress eCommerce infrastructure using AWS  S3 Bucket Technology and Cyber Panel And Light Speed Technologies(MVP)",
+      },
+      {
+        content:
+          " • Reviewing codes of React Frontend Part of Applications. Reviews and set up Sprint plans and goals.",
+      },
+    ],
+  },
+  {
+    header: " 👨‍💼 Business:",
+    dataContent: [
+      {
+        content:
+          " • Hiring Developers as well as Interviewing them if they fit the tech requirements.",
+      },
+      {
+        content:
+          " • Develop technical aspects of the company’s strategy to ensure alignment with its business goals",
+      },
+      {
+        content:
+          " • Discover and implement new technologies that yield a competitive advantage",
+      },
+      { content: " • Help departments use technology profitably" },
+      {
+        content:
+          "• Supervise system infrastructure to ensure functionality and efficiency",
+      },
+      { content: "• Build quality assurance and data protection processes" },
+      {
+        content:
+          "• Monitor KPIs and IT budgets to assess technological performance",
+      },
+      {
+        content:
+          "• Use stakeholders’ feedback to inform necessary improvements and adjustments to technology",
+      },
+      {
+        content: "• Communicate technology strategy to partners and investors",
+      },
+    ],
+  },
+];
+
+export default dataExperience;
