89e808010bde17cd7de7d00f75cf9d0f2fbc55ac
trieu nguyen <trieunguyen912@gmail.com>
2023-08-24 17:00:46 +0700

    refactor(page): add loading page

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,16 @@
 "use client";
 import { Sora } from "next/font/google";
 import "./globals.css";
+import Loading from "../components/loading-page";
 const sora = Sora({ subsets: ["latin"] });
+
 export default function MyApp({ Component, pageProps }) {
   return (
     <main className={sora.className}>
-      <Component {...pageProps} />
+      <Loading>
+        {" "}
+        <Component {...pageProps} />
+      </Loading>
     </main>
   );
 }
